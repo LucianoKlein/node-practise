442fb632381d32b8869aca76494844ed37c50dbc
LucianoKlein <kleinersilver@gmail.com>
2018-03-06 19:26:48 +0800

    practise

--- node7/server.js
+++ node7/server.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 
 let server = express();
 
 //cookie
-server.use(cookieParser("zlxkazxvadflkj"));
-
+;
+server.use(cookieParser());
+server.use(cookieSession({
+    keys: ["aaaa", "bbb", "ccc"],
+}));
 
 
 server.use("/aaa", function (req, resp) {
-   console.log(req.cookies);
-   req.secret = "zlxkazxvadflkj";
-   resp.cookie("password", "good", {signed: true});
-   resp.clearCookie("password");
-   console.log("签名cookie:" , req.signedCookies);
-   resp.send("ok");
+    if (req.session["count"] === null) {
+        req.session["count"] = 0;
+    } else {
+        req.session["count"]++;
+    }
+    console.log(req.session["count"]);
+    resp.send("ok");
 });
 server.listen(8080);
